032ecf905a481542904ec127450c3dad76486361
caioe <caioe@Caio>
2019-06-04 20:27:59 -0300

    Criação do pacote de serviço

--- artorys/src/main/java/br/com/artorys/servicos/UsuarioServico.java
+++ artorys/src/main/java/br/com/artorys/servicos/UsuarioServico.java
@@ -1,5 +1,23 @@
 package br.com.artorys.servicos;
 
+import br.com.artorys.interfaces.Entidade;
+
 public class UsuarioServico {
 
+	public void Insert(Entidade entidade) {
+
+	}
+
+	public void Update(Entidade entidade) {
+
+	}
+
+	public void Delete(Entidade entidade) {
+
+	}
+
+	public Entidade Select(Entidade entidade) {
+
+		return null;
+	}
 }
