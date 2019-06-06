7f37ac3e35d0a59e5c6614b7f42ee0fe74a2fddf
caioenio <caioenio6@gmail.com>
2019-06-06 13:55:56 -0300

    Criacão da ServletLogin

---
+++ artorys/src/main/java/br/com/artorys/servlets/ServletLogin.java
@@ -0,0 +1,36 @@
+package br.com.artorys.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ServletLogin() {
+		super();
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("email");
+		String password = request.getParameter("senha");
+
+		if (name.equals("caio") && password.equals("caio")) {
+			request.getRequestDispatcher("/front/login.html").include(request, response);
+		} else {
+			PrintWriter out = response.getWriter();
+			response.setContentType("text/html");
+			out.println("<script type=\"text/javascript\">");
+			out.println("alert('Email ou senha incorretos');");
+			out.println("</script>");
+			request.getRequestDispatcher("/front/home.html").include(request, response);
+		}
+	}
+
+}
