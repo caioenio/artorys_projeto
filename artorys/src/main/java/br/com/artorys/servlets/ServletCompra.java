496d0d88db9c0c14a81b91a96eeb892358408d68
caioenio <caioenio6@gmail.com>
2019-06-07 15:13:27 -0300

    Nesse commit foi feito a ServletCompra para informar uma mensagme caso o cliente não esteja logado

---
+++ artorys/src/main/java/br/com/artorys/servlets/ServletCompra.java
@@ -0,0 +1,30 @@
+package br.com.artorys.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/compra")
+public class ServletCompra extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+    public ServletCompra() {
+        super();
+      
+    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("<script type=\"text/javascript\">");
+		out.println("alert('Por favor, realize login para comprar');");
+		out.println("</script>");
+		request.getRequestDispatcher("/front/home.html").include(request, response);
+	}
+
+}
